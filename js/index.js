69fbc2e4b82fd86e8db241bbd5e8132edbda4103
unknown <trinhquochai12@gmail.com>
2021-08-11 01:22:38 +0700

    add file

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.onload=()=>{
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyBhtH7r3RHIklfKsZobTmXn59Dd6JcPidk",
+    authDomain: "chat-app-4f9a2.firebaseapp.com",
+    projectId: "chat-app-4f9a2",
+    storageBucket: "chat-app-4f9a2.appspot.com",
+    messagingSenderId: "240007223929",
+    appId: "1:240007223929:web:8442e53ce52b452a1d92a8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //console.log(firebase.app());
+  //khi chay ham lan dau,khi login,khi logout
+  
+  view.setActiveScreen('registerPage')
+
+}
